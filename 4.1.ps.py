47c96a37c8c419091771a9535e96ee0ef720ffba
nikhilthegreat1 <154723395+nikhilthegreat1@users.noreply.github.com>
2026-01-02 22:12:39 +0530

    tuple

---
+++ 4.1.ps.py
@@ -0,0 +1,11 @@
+a = int(input("enter marks of student one"))
+b = int(input("enter marks of student two"))
+c = int(input("enter marks of student three"))
+d = int(input("enter marks of student four"))
+e = int(input("enter marks of student five"))
+f = int(input("enter marks of student six"))
+g = int(input("enter marks of student seven"))
+list=[a,b,c,d,e,f,g]
+
+print(list)
+print(sum(list))
--- 41.ps.py
+++
@@ -1,9 +0,0 @@
-a = input("enter fruit one")
-b = input("enter fruit two")
-c = input("enter fruit three")
-d = input("enter fruit four")
-e = input("enter fruit five")
-f = input("enter fruit six")
-g = input("enter fruit seven")
-list=[a,b,c,d,e,f,g]
-print(list)
