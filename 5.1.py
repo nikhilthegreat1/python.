21606fbe5cbeff0bfb93c1073d89631e86757ccd
nikhilthegreat1 <154723395+nikhilthegreat1@users.noreply.github.com>
2026-01-03 11:00:18 +0530

    set

---
+++ 5.1.py
@@ -0,0 +1,4 @@
+s1 = {1,4,3,7}
+s2 = {3,5, 9, 11}
+print(s1.union(s2))
+print(s1.intersection(s2))
---
+++ 5.py
@@ -0,0 +1,7 @@
+marks = {
+    "harry": 100,
+    "nikhil": 101,
+    "rohan": 23
+}
+print (marks, type(marks))
+print(marks["harry"])
