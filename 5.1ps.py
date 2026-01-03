24d8f04cea018acdf5814f64f744a99dd4db799f
nikhilthegreat1 <154723395+nikhilthegreat1@users.noreply.github.com>
2026-01-03 15:32:49 +0530

    set

---
+++ 5.1ps.py
@@ -0,0 +1,7 @@
+dict={
+    "HOW ARE YOU": "kaise ho",
+    "are you fine": "aap this ho",
+    "what is your name": "aap ka name kya hai",
+    "what will be your future plan": "aap ka furture plan kya hai"
+}
+print(dict["HOW ARE YOU"])
---
+++ 5.2ps.py
@@ -0,0 +1,10 @@
+a = int(input("enter marks of student one"))
+b = int(input("enter marks of student two"))
+c = int(input("enter marks of student three"))
+d = int(input("enter marks of student four"))
+e = int(input("enter marks of student five"))
+f = int(input("enter marks of student six"))
+g = int(input("enter marks of student seven"))
+set={a,b,c,d,e,f,g}
+
+print(set)
