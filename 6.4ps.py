ea0ab5b8023a3a2a001ebfc28738140a4496b38d
nikhilthegreat1 <154723395+nikhilthegreat1@users.noreply.github.com>
2026-01-04 12:01:29 +0530

    n

---
+++ 6.4ps.py
@@ -0,0 +1,7 @@
+l = ["harry", "nikhil", "rahul", "ankit"]
+name=input("enter your name")
+
+if(name in l):
+    print("your name is in the list")
+else:
+    print("your name is not in the list")
---
+++ 6.6ps.py
@@ -0,0 +1,6 @@
+post = "hey my name is nikhil, nikhil age is 22, nikhil is graduated"
+
+if("nikhil".lower() in post.lower()):
+    print("this post is talking about nikhil")
+else:
+    print("not talking about nIkhil")
